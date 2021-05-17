65bc6a1de702a01b45a74711a40073bd8c80c11c
Reynelbis Larquin Guerra <reynelbis@camaguey.geocuba.cu>
2021-05-16 20:37:32 -0400

    se borraron los anteriores servicios

--- src/security/repository/role.repository.ts
+++ src/security/repository/role.repository.ts
@@ -4,10 +4,6 @@ import {IRepository} from "../../shared/interface";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {RoleEntity} from "../entity";
-import {RoleMapper} from "../mapper";
-import {CreateRoleDto, UpdateRoleDto} from "../dto";
-import {IPaginationOptions, paginate, Pagination} from "nestjs-typeorm-paginate";
-import {status} from "../../shared/enum";
 
 @Injectable()
 export class RoleRepository extends GenericRepository<RoleEntity> implements IRepository<RoleEntity>{
@@ -15,48 +11,4 @@ export class RoleRepository extends GenericRepository<RoleEntity> implements IRe
                  private roleRepository: Repository<RoleEntity>){
         super(roleRepository);
     }
-    // constructor(
-    //     @InjectRepository(RoleEntity)
-    //     private roleRepository: Repository<RoleEntity>,
-    //     private roleMapper: RoleMapper,
-    // ) {
-    // }
-    //
-    // async getAll(options: IPaginationOptions): Promise<Pagination<RoleEntity>> {
-    //     return await paginate<RoleEntity>(this.roleRepository, options, {where: {status: status.ACTIVE}});
-    //
-    // }
-    //
-    // async get(id: number): Promise<RoleEntity> {
-    //     const rol: RoleEntity = await this.roleRepository.findOne(id,{
-    //         where: {status: status.ACTIVE}
-    //     });
-    //     return rol;
-    // }
-    //
-    // async create(createRoleDto: CreateRoleDto): Promise<RoleEntity> {
-    //     const newRole = this.roleMapper.dtoToEntity(createRoleDto);
-    //     return await this.roleRepository.save(newRole);
-    // }
-    //
-    // async update(id: number, updateRoleDto: UpdateRoleDto): Promise<RoleEntity> {
-    //     const foundRole: RoleEntity = await this.roleRepository.findOne(id);
-    //     if (!foundRole) {
-    //         throw new NotFoundException('No existe el rol');
-    //     }
-    //     const {nombre, descripcion} = updateRoleDto;
-    //     foundRole.nombre = nombre;
-    //     foundRole.descripcion = descripcion;
-    //     const updatedRole: RoleEntity = await this.roleRepository.save(foundRole);
-    //     return updatedRole;
-    // }
-    //
-    // async delete(id: number): Promise<void> {
-    //     const role = await this.roleRepository.findOne(id, {where: {status: status.ACTIVE}});
-    //     if (!role) {
-    //         throw new NotFoundException('No existe el rol');
-    //     }
-    //     role.status = status.INACTIVE;
-    //     await this.roleRepository.save(role);
-    // }
 }
--- src/security/service/role.service.ts
+++ src/security/service/role.service.ts
@@ -15,47 +15,4 @@ export class RoleService extends GenericService<RoleEntity> implements IService<
    ){
        super(roleRepository,roleMapper,trazaService,true);
    }
-
-    // constructor(
-    //     private roleRepository: RoleRepository,
-    //     private roleMapper: RoleMapper,
-    //     private trazaService: TrazaService,
-    // ) {
-    // }
-    //
-    // async getAll(options: IPaginationOptions): Promise<Pagination<ReadRoleDto>> {
-    //     const roles: Pagination<RoleEntity> = await this.roleRepository.getAll(options);
-    //     const readRoleDto: ReadRoleDto[] = roles.items.map((rol: RoleEntity) => this.roleMapper.entityToDto(rol));
-    //     return new Pagination(readRoleDto, roles.meta, roles.links);
-    // }
-    //
-    // async get(id: number): Promise<ReadRoleDto> {
-    //     if (!id) {
-    //         throw new BadRequestException("El id no puede ser vacio");
-    //     }
-    //     const rol: RoleEntity = await this.roleRepository.get(id);
-    //     if (!rol) {
-    //         throw new NotFoundException('El rol no se encuentra.');
-    //     }
-    //     return this.roleMapper.entityToDto(rol);
-    // }
-    //
-    // async create(user: UserEntity, createRoleDto: CreateRoleDto): Promise<ReadRoleDto> {
-    //     const roleEntity: RoleEntity = await this.roleRepository.create(createRoleDto);
-    //     await this.trazaService.create(user, roleEntity, HISTORY_ACTION.ADD);
-    //     return this.roleMapper.entityToDto(roleEntity);
-    // }
-    //
-    // async update(user: UserEntity, id: number, rol: UpdateRoleDto): Promise<ReadRoleDto> {
-    //     const roleEntity: RoleEntity = await this.roleRepository.update(id, rol);
-    //     await this.trazaService.create(user, roleEntity, HISTORY_ACTION.MOD);
-    //     return this.roleMapper.entityToDto(roleEntity);
-    //
-    // }
-    //
-    // async delete(user: UserEntity, id: number): Promise<void> {
-    //     const roleEntity: RoleEntity = await this.roleRepository.get(id);
-    //     await this.trazaService.create(user, roleEntity, HISTORY_ACTION.DEL);
-    //     return await this.roleRepository.delete(id);
-    // }
 }
