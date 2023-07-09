4c3d4de295df2e006f9f437665409d12a7acc20d
Reynelbis Larquin Guerra <rlarquing@gmail.com>
2023-07-09 18:41:45 -0400

    areglado el create de usuario

--- src/core/mapper/rol.mapper.ts
+++ src/core/mapper/rol.mapper.ts
@@ -2,19 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { FuncionMapper } from './funcion.mapper';
 import {
   FuncionRepository,
-  RolRepository,
-  UserRepository,
 } from '../../persistence/repository';
 import {
-  FuncionEntity,
-  GenericNomencladorEntity,
   RolEntity,
-  UserEntity,
 } from '../../persistence/entity';
 import {
   CreateRolDto,
   ReadFuncionDto,
-  ReadNomencladorDto,
   ReadRolDto,
   SelectDto,
   UpdateRolDto,
@@ -23,23 +17,12 @@ import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class RolMapper {
-  constructor(protected funcionMapper: FuncionMapper) {}
+  constructor(
+      protected funcionMapper: FuncionMapper,
+      protected funcionRepository: FuncionRepository
+              ) {}
   dtoToEntity(createRolDto: CreateRolDto): RolEntity {
     return plainToInstance(RolEntity, createRolDto);
-    // let users: UserEntity[] = [];
-    // if (createRolDto.users != undefined) {
-    //   users = await this.userRepository.findByIds(createRolDto.users);
-    // }
-    // let funcions: FuncionEntity[] = [];
-    // if (createRolDto.funcions != undefined) {
-    //   funcions = await this.funcionRepository.findByIds(createRolDto.funcions);
-    // }
-    // return new RolEntity(
-    //   createRolDto.nombre,
-    //   createRolDto.descripcion,
-    //   funcions,
-    //   users,
-    // );
   }
   dtoToUpdateEntity(
     updateRolDto: UpdateRolDto,
@@ -49,55 +32,22 @@ export class RolMapper {
       ...updateRolEntity,
       ...updateRolDto,
     });
-    // if (updateRolDto.users != undefined) {
-    //   const users: UserEntity[] = await this.userRepository.findByIds(
-    //     updateRolDto.users,
-    //   );
-    //   if (users) {
-    //     updateRolEntity.users = users;
-    //   }
-    // }
-    // if (updateRolDto.funcions !== undefined) {
-    //   updateRolEntity.funcions = await this.funcionRepository.findByIds(
-    //     updateRolDto.funcions,
-    //   );
-    // }
-    //
-    // updateRolEntity.nombre = updateRolDto.nombre;
-    // updateRolEntity.descripcion = updateRolDto.descripcion;
-    // return updateRolEntity;
   }
   async entityToDto(rolEntity: RolEntity): Promise<ReadRolDto> {
     const readRolDto: ReadRolDto = plainToInstance(ReadRolDto, rolEntity);
     readRolDto.dtoToString = rolEntity.toString();
     const selectUserDto: SelectDto[] = [];
+
     for (const user of rolEntity.users) {
       selectUserDto.push({label: user.username, value: user.id});
     }
     const readFuncionDto: ReadFuncionDto[] = [];
     for (const funcion of rolEntity.funcions) {
-      readFuncionDto.push(await this.funcionMapper.entityToDto(funcion));
+      const funcionRol = await this.funcionRepository.findById(funcion.id);
+      readFuncionDto.push(await this.funcionMapper.entityToDto(funcionRol));
     }
     readRolDto.users = selectUserDto;
     readRolDto.funcions = readFuncionDto;
     return readRolDto;
-    //   const rol: RolEntity = await this.rolRepository.findById(rolEntity.id);
-    //   const selectUserDto: SelectDto[] = [];
-    //   for (const user of rol.users) {
-    //     selectUserDto.push({ label: user.username, value: user.id });
-    //   }
-    //   const readFuncionDto: ReadFuncionDto[] = [];
-    //   for (const funcion of rol.funcions) {
-    //     readFuncionDto.push(await this.funcionMapper.entityToDto(funcion));
-    //   }
-    //   const dtoToString: string = rol.toString();
-    //   return new ReadRolDto(
-    //     dtoToString,
-    //     rolEntity.id,
-    //     rolEntity.nombre,
-    //     rolEntity.descripcion,
-    //     selectUserDto,
-    //     readFuncionDto,
-    //   );
   }
 }
--- src/core/mapper/user.mapper.ts
+++ src/core/mapper/user.mapper.ts
@@ -10,12 +10,15 @@ import {
 } from '../../shared/dto';
 import { UserEntity } from '../../persistence/entity';
 import { plainToInstance } from 'class-transformer';
+import {FuncionRepository, RolRepository} from "../../persistence/repository";
 
 @Injectable()
 export class UserMapper {
   constructor(
     protected rolMapper: RolMapper,
     protected funcionMapper: FuncionMapper,
+    protected rolRepository: RolRepository,
+    protected funcionRepository: FuncionRepository,
   ) {}
   dtoToEntity(userDto: UserDto): UserEntity {
     return plainToInstance(UserEntity, userDto);
@@ -35,11 +38,13 @@ export class UserMapper {
 
     const readRolDto: ReadRolDto[] = [];
     for (const rol of userEntity.roles) {
-      readRolDto.push(await this.rolMapper.entityToDto(rol));
+      const rolUser=await this.rolRepository.findById(rol.id);
+      readRolDto.push(await this.rolMapper.entityToDto(rolUser));
     }
     const readFuncionDto: ReadFuncionDto[] = [];
     for (const funcion of userEntity.funcions) {
-      readFuncionDto.push(await this.funcionMapper.entityToDto(funcion));
+      const funcionUser=await this.funcionRepository.findById(funcion.id);
+      readFuncionDto.push(await this.funcionMapper.entityToDto(funcionUser));
     }
     readUserDto.roles = readRolDto;
     readUserDto.funcions = readFuncionDto;
